0fe179820ca9032db3264e0f14f0c1271cd2f89e
Abhinav Sharma <abhi5237@gmail.com>
2024-10-12 21:45:31 +0530

    frontend update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,17 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import { Footer, Transactions, Welcome, Navbar, Services } from "./components"
 
 
 const App = () => {
   return (
-    <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
       </div>
-    </>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
   )
 }
 
